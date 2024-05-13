f6e5b8889e0e05a05b6fa0a134c7734884a0be26
Milosz Bukala <milosz.bukala@giraffestudio.pl>
2024-05-13 18:03:08 +0200

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,15 @@
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        repellat velit, minus voluptas aliquid explicabo sit soluta deleniti eum
+        ullam nobis fugit? Repudiandae quis maiores quidem amet possimus iste
+        ad.
+      </p>
+    </>
+  )
+}
+
+export default HomePage
